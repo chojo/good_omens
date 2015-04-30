39cb04e6885487c8f4452479c40091577cceb9e6
Joon Ki Choi <joon.ch@gmail.com>
2015-04-30 19:47:50 +0200

    add example project for clojure & portable Java application generation

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject lein-cljsbuild-example "1.2.3"
+  :description "A simple example of how to use OM"
+  :plugins [[lein-cljsbuild "1.0.5"]]
+  :dependencies [[org.clojure/clojurescript "0.0-3211"]])
